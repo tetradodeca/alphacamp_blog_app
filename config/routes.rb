89982aaa9558cda7abf093ec147bbf43c152f8cd
sha <urabummer4@hotmail.com>
2017-09-02 19:13:53 +0800

    class practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
  
- root to: 'pages#home'
+ # root to: 'pages#home'
+ root to: "posts#index"
 
     get 'about', to: 'pages#about'  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get 'services', to: 'pages#services'
     get 'home', to: 'pages#home'
+
+    # get 'post/new', to: 'posts#new'
+    # post 'post', to: 'posts#create'
+
+    resources :posts
 end
